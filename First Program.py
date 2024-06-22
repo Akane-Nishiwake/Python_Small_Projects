ccf1c43278cda0aa4fd96c80834d2e0bca596ac8
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2024-06-22 17:09:47 -0400

    Update First Program.py

--- First Program.py
+++ First Program.py
@@ -1,10 +1,10 @@
 # Write your code here :-)
 print("Hello world!")
 print("What is your name?")
-myName = input()  # idk why this is only called input and i hate it
+myName = input()  #taking in user input
 print("It is nice to meet you " + myName)
 print("The length of your name is: ")
-print(len(myName))
+print(len(myName)) #printing out user input
 print("What is your age?")
 myAge = input()
 print("You will be " + str(int(myAge) + 1) + " in a year")
