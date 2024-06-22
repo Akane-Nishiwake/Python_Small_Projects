58e8430538276b7ecd7b475298c30706ad88f104
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2024-06-22 17:18:11 -0400

    Create Conway's GOL.py

---
+++ Conway's GOL.py
@@ -0,0 +1,7 @@
+# Write your code here :-)
+#Conway's Game of Life
+#The 4 Rules of a cell
+#Any live cell with 0 or 1 live neighbors becomes dead, because of underpopulation
+#Any live cell with 2 or 3 live neighbors stays alive, because its neighborhood is just right
+#Any live cell with more than 3 live neighbors becomes dead, because of overpopulation
+#Any dead cell with exactly 3 live neighbors becomes alive, by reproduction
