7cda8ae4731b257ad0eecff301238f9e8171f1e8
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2024-06-24 01:46:29 -0400

    Update Conway's GOL.py

--- Conway's GOL.py
+++ Conway's GOL.py
@@ -9,24 +9,3 @@
 # Any dead cell with exactly 3 live neighbors becomes alive, by reproduction
 import random
 
-# This is the base Board state of a 10 x 10
-board_state = [
-    [1, 1, 1, 1, 1, 1, 1],#[0][0-6]
-    [1, 1, 1, 1, 1, 1, 1],#[1][0-6]
-    [1, 1, 1, 1, 1, 1, 1],#[2][0-6]
-    [1, 1, 1, 1, 1, 1, 1],#[3][0-6]
-    [1, 1, 1, 1, 1, 1, 1],#[4][0-6]
-    [1, 1, 1, 1, 1, 1, 1],#[6][0-6]
-    [1, 1, 1, 1, 1, 1, 1],#[7][0-6]
-    [1, 1, 1, 1, 1, 1, 1],#[8][0-6]
-    [1, 1, 1, 1, 1, 1, 1] #[9][0-6]
-]
-
-#board dead state where all values are 0
-def deadState(_width, _height):
-
-
-
-#board
-width = 10
-height = 10
