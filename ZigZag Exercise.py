65e05968ecb200bba9f19b9acb4b64fff9a7cd70
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2024-06-24 13:51:48 -0400

    Update ZigZag Exercise.py

--- ZigZag Exercise.py
+++ ZigZag Exercise.py
@@ -1,1 +1,19 @@
 # Write your code here :-)
+import time, sys
+indent = 0 #spaces to indent
+isIndentIncrease = True #does the indent increase or not
+try:
+    while True:
+        print(' ' * indent, end='')
+        print('*******')
+        time.sleep(0.1)
+
+        if isIndentIncrease:
+            indent = indent + 1
+            if indent == 20:
+                isIndentIncrease = False
+
+        else:
+            indent = indent - 1
+            if indent = 0:
+
