8cf9457fa931193b09d1eb2c3a0773b9f02cd17e
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2024-06-24 22:17:53 -0400

    Update ZigZag Exercise.py

--- ZigZag Exercise.py
+++ ZigZag Exercise.py
@@ -1,19 +1,22 @@
 # Write your code here :-)
 import time, sys
-indent = 0 #spaces to indent
-isIndentIncrease = True #does the indent increase or not
+
+indent = 0  # spaces to indent
+isIndentIncrease = True  # does the indent increase or not
 try:
     while True:
-        print(' ' * indent, end='')
-        print('*******')
-        time.sleep(0.1)
+        print(" " * indent, end="")
+        print("*******")
+        time.sleep(0.1)#allows for the program to run smoothly
 
         if isIndentIncrease:
             indent = indent + 1
             if indent == 20:
-                isIndentIncrease = False
+                isIndentIncrease = False#Change direction
 
         else:
             indent = indent - 1
-            if indent = 0:
-
+            if indent == 0:
+                isIndentIncrease = True#Change direction
+except KeyboardInterrupt:#Ctrl + C to interrupt program
+    sys.exit()#exit program
