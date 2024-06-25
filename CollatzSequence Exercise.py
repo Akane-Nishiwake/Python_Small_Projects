30a20341b326a38beb462baa21e3841b3a6f73eb
Amara Gitomer <59937361+Akane-Nishiwake@users.noreply.github.com>
2024-06-25 09:36:17 -0400

    Update CollatzSequence Exercise.py

--- CollatzSequence Exercise.py
+++ CollatzSequence Exercise.py
@@ -1,1 +1,23 @@
 # Write your code here :-)
+import sys
+
+def collatz(number):
+    if number % 2 == 0:
+        num = number // 2
+        print(num)
+        return num
+    else:
+        num = 3 * number + 1
+        print(num)
+        return num
+
+
+print("Please enter a number: ")
+while True:
+    try:
+        userInput = int(input())
+        while userInput != 1:
+            userInput = collatz(userInput)
+        break
+    except ValueError:
+        print("Please enter a valid integer:")
